bc1fa0752c09a88495e78bd54afb9b2e406bf16e
Lukas Lehmann <lukaslehmann05@gmail.com>
2026-05-20 15:03:53 +0200

    added nav bar highlight when scrolling, adjusted english index

---
+++ js/section_highlight.js
@@ -0,0 +1,18 @@
+let allSections = document.querySelectorAll('[id*="highlight"]')
+
+function checkCurrentScroll() {
+    let scrollY = window.scrollY
+
+    allSections.forEach(element => {
+        let sectionPos = element.getBoundingClientRect()
+        let currentOffset = window.innerHeight / 2 + scrollY
+        let elementID = element.id
+        let filtered_element = elementID.replace("highlight", "").trim()
+        
+        if (currentOffset >= sectionPos.top + scrollY && currentOffset <= sectionPos.bottom + scrollY) {
+            document.getElementById(`nav_${filtered_element}`).classList.add('current-focus')
+        } else {
+            document.getElementById(`nav_${filtered_element}`).classList.remove('current-focus')
+        }
+    })
+}
