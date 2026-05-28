5be926864500e2d2fcf179e0912c01c8f989332b
Lukas Lehmann <lukaslehmann05@gmail.com>
2026-05-28 13:02:16 +0200

    added fade in effect for elements when scrolling

---
+++ js/observer_handler.js
@@ -0,0 +1,53 @@
+const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0
+}
+
+const optionsSkills = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0
+}
+
+const observer = new IntersectionObserver(handleIntersect, options)
+const observerSkills = new IntersectionObserver(handleIntersectLongAnim, optionsSkills)
+
+function handleIntersect(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.remove("fade-init")
+            entry.target.classList.add("fade")
+        } else {
+            entry.target.classList.remove("fade")
+            entry.target.classList.add("fade-init")
+        }
+    })
+}
+
+function handleIntersectLongAnim(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.remove("fade-long-init")
+            entry.target.classList.add("fade-long")
+        } else {
+            entry.target.classList.remove("fade-long")
+            entry.target.classList.add("fade-long-init")
+        }
+    })
+}
+
+function observeAll(selector, activeObserver, initialClass) {
+    document.querySelectorAll(selector).forEach((element) => {
+        element.classList.add(initialClass)
+        activeObserver.observe(element)
+    })
+}
+
+observeAll(".project-card", observer, "fade-init")
+observeAll(".contact-section-title", observer, "fade-init")
+observeAll(".contact-info", observer, "fade-init")
+observeAll(".contact-form", observer, "fade-init")
+observeAll(".language-subtext", observerSkills, "fade-init")
+observeAll(".feedback-cards", observer, "fade-init")
+observeAll(".language-card", observerSkills, "fade-long-init")
