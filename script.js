22b50d1b7eccd3cd72c205b3bf60f4280b1d1e39
Lukas Lehmann <lukaslehmann05@gmail.com>
2026-06-15 10:16:47 +0200

    fixed header moving content, changed spacing at project section and aligned about icons in responsive view

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const burgerClosed = "../img/icons/burger_closed.svg"
 const burgerOpen = "../img/icons/burger_opened.svg"
 const burgerMenu = document.getElementById("burger_menu")
 const nav_bar = document.getElementById("nav_bar")
-const burgerIconAlignTop = "flex-start"
-const burgerIconAlignCenter = "center"
 const headerContent = document.querySelector(".header-content")
 const locationText = document.getElementById("location")
 const relocateText = document.getElementById("relocate")
@@ -30,9 +28,40 @@ function checkPreferedLanguage() {
     }
 }
 
+/**
+ * This function toggles the burger menu
+ */
 function toggleBurgerMenu() {
     nav_bar.classList.toggle("show-burger-nav")
     burgerMenuOpen = !burgerMenuOpen
     burgerMenu.src = burgerMenuOpen ? burgerOpen : burgerClosed
-    headerContent.style.alignItems = burgerMenuOpen ? burgerIconAlignTop : burgerIconAlignCenter
+    headerContent.style.alignItems = burgerMenuOpen ? "flex-start" : "center"
+    document.querySelector('header').style.position = burgerMenuOpen ? checkIfScrolled() : "sticky"
+    document.querySelector('main').style.marginTop = burgerMenuOpen ? checkForMargin() : "0px"
+    document.querySelector('#burger_menu').style.paddingTop = burgerMenuOpen ? "11px" : "0px"
+    
+}
+
+/**
+ * This function checks how much was scrolled so that the style can be changed accordingly.
+ * @returns {string} The position of the header, either "absolute" or "sticky"
+ */
+function checkIfScrolled() {
+    if (window.scrollY <= 50) {
+        return "absolute"
+    } else {
+        return "sticky"
+    }
+}
+
+/**
+ * This function checks how much was scrolled so that the style can be changed accordingly.
+ * @returns {string} The margin-top value for the main content
+ */
+function checkForMargin() {
+    if (window.scrollY <= 50) {
+        return "101px"
+    } else {
+        return "0px"
+    }
 }
