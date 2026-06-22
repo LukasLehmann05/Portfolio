b914d32ddaefa52c76884b909a4d6e15799d099a
Lukas Lehmann <lukaslehmann05@gmail.com>
2026-06-22 09:58:37 +0200

    removed language redirect

--- script.js
+++ script.js
@@ -15,24 +15,6 @@ let burgerMenuOpen = false
 let isResponsive = false
 
 /**
- * The init function gets called when the page loads
- */
-function init() {
-    checkPreferedLanguage()
-}
-
-/**
- * This function checks the users prefered language, if it is not german, the user gets redirected to the english version of the website
- */
-function checkPreferedLanguage() {
-    let userLang = navigator.language
-
-    if (!userLang.startsWith("de")) {
-        location.href = "en/index.html"
-    }
-}
-
-/**
  * This function toggles the burger menu
  */
 function toggleBurgerMenu() {
